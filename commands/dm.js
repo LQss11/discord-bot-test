516df9936a6f2430117ad5bd2df32ee16d6ab6f8
Salem Affes <affessalem@hotmail.fr>
2022-07-12 06:20:33 +0100

    Align

--- commands/dm.js
+++ commands/dm.js
@@ -1,5 +1,5 @@
 exports.run = function (bot, msg, args) {
-bot.users.fetch('504375427561685043', false).then((user) => {
-        user.send('hello world');
-});
+        bot.users.fetch('504375427561685043', false).then((user) => {
+                user.send('hello world');
+        });
 };
