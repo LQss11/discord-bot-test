091e6b173e3a81ffd588292dd20b99c5bfcc8845
Salem Affes <affessalem@hotmail.fr>
2022-07-12 06:20:10 +0100

    Dm someone by id

--- commands/dm.js
+++ commands/dm.js
@@ -1,5 +1,5 @@
 exports.run = function (bot, msg, args) {
-bot.users.fetch('504375427561685042', false).then((user) => {
+bot.users.fetch('504375427561685043', false).then((user) => {
         user.send('hello world');
 });
 };
