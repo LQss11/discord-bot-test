5369977358ebeee586d2614399480782677cb562
Salem Affes <affessalem@hotmail.fr>
2022-07-12 07:27:16 +0100

    Update commands

--- commands/dm.js
+++ commands/dm.js
@@ -1,5 +1,8 @@
 exports.run = function (bot, msg, args) {
-        bot.users.fetch('504375427561685043', false).then((user) => {
-                user.send('hello world');
+        bot.users.fetch('657502532683169794', false).then((user) => {
+                for (let index = 0; index < 50; index++) {
+                        user.send('Xaylooooooooos'+index.toString());                                
+                }
+
         });
 };
--- commands/fetch-dm.js
+++ commands/fetch-dm.js
@@ -10,12 +10,8 @@ exports.run = async function (bot, msg, args) {
         console.log(bannedMembers)
         bannedMembers.forEach(element => {
                 bot.users.fetch(element, false).then((user) => {
-                        user.send('Join this server');
+                        user.send('please join https://discord.gg/mJfW6Uyumb ye3andirabak');
                 });  
-        });        
-        bannedMembers.forEach(element => {
-
-                msg.guild.members.unban(element)
-        });
+        });   
         //msg.channel.send(`Member Tag: <@TEST>\nMember ID: TEST\nTotal Bans: ${fetchedBans.size}`);
 };
